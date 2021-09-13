b3c18b65a3bf1cfc5d527f8cbcf39a41e8dcc043
wzzzxhui <1976412306@qq.com>
2021-09-13 16:52:16 +0800

    管理后台 - 用户列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,23 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use DefaultDatetimeFormat;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
+
     protected $fillable = [
         'name',
         'email',
